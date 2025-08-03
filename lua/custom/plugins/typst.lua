05ff6f3422e2966530fa563ba56608594daf732b
Ian Snider <iansnider@Mac.attlocal.net>
2025-08-02 18:19:47 -0700

    feat(typst): Added file exporting fuzzy finder - fuzzy finder support pdf, svg, png, and html

--- lua/custom/plugins/typst.lua
+++ lua/custom/plugins/typst.lua
@@ -7,7 +7,7 @@ return {
   end,
   opts = {
     follow_cursor = true,
-    open_cmd = nil, -- default to browser
+    open_cmd = nil, -- default browse
   },
   config = function(_, opts)
     require('typst-preview').setup(opts)
@@ -19,14 +19,9 @@ return {
       local conf = require('telescope.config').values
       local actions = require 'telescope.actions'
       local action_state = require 'telescope.actions.state'
-
       local preview_options = {
         { name = 'Browser', cmd = nil, desc = 'Preview in default browser' },
-        { name = 'Zathura', cmd = 'zathura %s', desc = 'Preview in Zathura PDF viewer' },
-        { name = 'Firefox', cmd = 'firefox %s', desc = 'Preview in Firefox' },
-        { name = 'Chrome', cmd = 'google-chrome %s', desc = 'Preview in Chrome' },
       }
-
       pickers
         .new({}, {
           prompt_title = 'Typst Preview Method',
@@ -41,13 +36,11 @@ return {
             end,
           },
           sorter = conf.generic_sorter {},
-          attach_mappings = function(prompt_bufnr, map)
+          attach_mappings = function(prompt_bufnr)
             actions.select_default:replace(function()
               actions.close(prompt_bufnr)
               local selection = action_state.get_selected_entry()
               local option = selection.value
-
-              -- Configure and start preview
               require('typst-preview').setup {
                 open_cmd = option.cmd,
                 follow_cursor = true,
@@ -60,14 +53,69 @@ return {
         :find()
     end
 
-    -- Create the command
-    vim.api.nvim_create_user_command('TypstPreviewSelect', typst_preview_picker, {})
+    -- Telescope picker for export options
+    local function typst_export_picker()
+      local pickers = require 'telescope.pickers'
+      local finders = require 'telescope.finders'
+      local conf = require('telescope.config').values
+      local actions = require 'telescope.actions'
+      local action_state = require 'telescope.actions.state'
 
-    -- Optional: Add keybinding for typst files only
+      local export_options = {
+        { name = 'PDF', ext = 'pdf', desc = 'Export to PDF' },
+        { name = 'SVG', ext = 'svg', desc = 'Export to SVG' },
+        { name = 'PNG', ext = 'png', desc = 'Export to PNG' },
+        { name = 'HTML', ext = 'html', desc = 'Export to HTML' },
+      }
+
+      pickers
+        .new({}, {
+          prompt_title = 'Export Typst Document',
+          finder = finders.new_table {
+            results = export_options,
+            entry_maker = function(entry)
+              return {
+                value = entry,
+                display = entry.name .. ' - ' .. entry.desc,
+                ordinal = entry.name,
+              }
+            end,
+          },
+          sorter = conf.generic_sorter {},
+          attach_mappings = function(prompt_bufnr)
+            actions.select_default:replace(function()
+              actions.close(prompt_bufnr)
+              local selection = action_state.get_selected_entry()
+              local option = selection.value
+
+              -- Get current file name without extension
+              local current_file = vim.fn.expand '%:r' -- removes extension
+              local output_file = current_file .. '.' .. option.ext
+              local input_file = vim.fn.expand '%' -- current file with extension
+
+              -- Run typst compile command
+              local cmd = string.format('typst compile %s %s', input_file, output_file)
+              vim.fn.system(cmd)
+
+              -- Show result
+              print(string.format('Exported to: %s', output_file))
+            end)
+            return true
+          end,
+        })
+        :find()
+    end
+
+    -- Create commands
+    vim.api.nvim_create_user_command('TypstPreviewSelect', typst_preview_picker, {})
+    vim.api.nvim_create_user_command('TypstExport', typst_export_picker, {})
+
+    -- Create keybindings
     vim.api.nvim_create_autocmd('FileType', {
       pattern = 'typst',
       callback = function()
         vim.keymap.set('n', '<leader>tp', ':TypstPreviewSelect<CR>', { desc = 'Select Typst Preview Method' })
+        vim.keymap.set('n', '<leader>te', ':TypstExport<CR>', { desc = 'Export Typst Document' })
       end,
     })
   end,
